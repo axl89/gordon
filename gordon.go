cb59688fe6ed2106d1fdd11536a32116562e4b11
Axel Amigo Arnold <axl89@users.noreply.github.com>
2020-04-03 13:57:08 +0200

    Execute the function in all regions

--- gordon.go
+++ gordon.go
@@ -20,17 +20,37 @@ func main() {
 
 	var data = engine.GetJSON()
 
-	var clientEU = GetLambdaClient("eu-west-1")
-	var clientUS = GetLambdaClient("us-east-1")
-
 	payload, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println("Error marshalling Pinguer request")
 		os.Exit(1)
 	}
 
-	clientEU.Invoke(&lambda.InvokeInput{InvocationType: aws.String("Event"), FunctionName: aws.String("ping-as-a-service-aws-lambda-production-main"), Payload: payload})
-	clientUS.Invoke(&lambda.InvokeInput{InvocationType: aws.String("Event"), FunctionName: aws.String("ping-as-a-service-aws-lambda-production-main"), Payload: payload})
+	regions := []string{
+		"eu-north-1",
+		"ap-south-1",
+		"eu-west-3",
+		"eu-west-2",
+		"eu-west-1",
+		"ap-northeast-2",
+		"ap-northeast-1",
+		"sa-east-1",
+		"ca-central-1",
+		"ap-southeast-1",
+		"ap-southeast-2",
+		"eu-central-1",
+		"us-east-1",
+		"us-east-2",
+		"us-west-1",
+		"us-west-2",
+	}
+
+	var client *lambda.Lambda
+
+	for _, regionName := range regions {
+		client = GetLambdaClient(regionName)
+		client.Invoke(&lambda.InvokeInput{InvocationType: aws.String("Event"), FunctionName: aws.String("pinger-lambda-production-main"), Payload: payload})
+	}
 
 	fmt.Println("Done")
 
